245ec69def0aefd467a4f7f16c36bad2540acabd
Mykolas Kairys <mykolas.kairys@vinted.com>
2020-12-06 21:17:17 +0200

    fix for vw and vh for mobile web

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,25 @@
-import React, {Suspense} from "react"
+import React, {Suspense, useEffect} from "react"
 import ReactDOM from "react-dom"
 
 import Layout from "./Layout"
 import GraphQLProvider from "./GraphQLProvider"
 import {BrowserRouter} from "react-router-dom"
 
-const App = () => (
-    <GraphQLProvider>
-        <BrowserRouter>
-            <Layout />
-        </BrowserRouter>
-    </GraphQLProvider>
-)
+const App = () => {
+    useEffect(() => {
+        const vh = window.innerHeight * 0.01
+        const vw = window.innerWidth * 0.01
+        window.document.documentElement.style.setProperty("--vh", `${vh}px`)
+        window.document.documentElement.style.setProperty("--vw", `${vw}px`)
+    })
+
+    return (
+        <GraphQLProvider>
+            <BrowserRouter>
+                <Layout />
+            </BrowserRouter>
+        </GraphQLProvider>
+    )
+}
 
 ReactDOM.render(<App />, document.getElementById("root"))
