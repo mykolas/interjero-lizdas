1658f6217c3a76eb34744a1be8b352df948ed3d9
Mykolas Kairys <mykolas.kairys@vinted.com>
2020-12-07 20:22:16 +0200

    logo navigates to home

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,13 +1,20 @@
-import React from "react"
+import React, {useCallback} from "react"
 import styles from "../App.module.scss"
+import {useHistory} from "react-router-dom"
 
-const Logo: React.SFC = () => (
-    <img
-        height={50}
-        width={310}
-        className={styles.logo}
-        src="https://cdn.sanity.io/images/pzhvpnrz/production/8dc618a42ad6db4c3b1d283b52781b4d8c6711df-1080x372.png?h=160&fm=webp"
-    />
-)
+const Logo: React.SFC = () => {
+    const history = useHistory()
+    const handleOnClick = useCallback(() => history.push("/", {}), [history])
+
+    return (
+        <img
+            height={50}
+            width={310}
+            className={styles.logo}
+            onClick={handleOnClick}
+            src="https://cdn.sanity.io/images/pzhvpnrz/production/8dc618a42ad6db4c3b1d283b52781b4d8c6711df-1080x372.png?h=160&fm=webp"
+        />
+    )
+}
 
 export default Logo
