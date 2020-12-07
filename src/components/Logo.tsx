c18afbebfd0534f65b7fafc9496c3e3cd008e163
Mykolas Kairys <mykolas.kairys@vinted.com>
2020-12-07 18:57:09 +0200

    explicit width for logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,6 +4,7 @@ import styles from "../App.module.scss"
 const Logo: React.SFC = () => (
     <img
         height={50}
+        width={310}
         className={styles.logo}
         src="https://cdn.sanity.io/images/pzhvpnrz/production/8dc618a42ad6db4c3b1d283b52781b4d8c6711df-1080x372.png?h=160&fm=webp"
     />
