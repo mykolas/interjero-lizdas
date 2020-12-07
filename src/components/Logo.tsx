b30c76651fe691f42280bacd2ea646fc17a233d1
Mykolas Kairys <mykolas.kairys@vinted.com>
2020-12-07 14:31:30 +0200

    use webp for logo instead of png

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ import styles from "../App.module.scss"
 const Logo: React.SFC = () => (
     <img
         className={styles.logo}
-        src="https://cdn.sanity.io/images/pzhvpnrz/production/8dc618a42ad6db4c3b1d283b52781b4d8c6711df-1080x372.png?h=160"
+        src="https://cdn.sanity.io/images/pzhvpnrz/production/8dc618a42ad6db4c3b1d283b52781b4d8c6711df-1080x372.png?h=160&fm=webp"
     />
 )
 
