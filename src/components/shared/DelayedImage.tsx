a2288edb3f1ea0ae2ceccb5d5476210bda4d67d8
Mykolas Kairys <mykolas.kairys@vinted.com>
2020-12-09 13:17:36 +0200

    spread image props in delayed image

---
+++ src/components/shared/DelayedImage.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import {useDelayedVisibility} from "./hooks/useDelayedVisibility"
+
+const LazyImage: React.FC<
+    React.ImgHTMLAttributes<HTMLImageElement> & {src?: string; delayInMilliseconds?: number}
+> = ({delayInMilliseconds, ...props}) => {
+    const isVisible = useDelayedVisibility(delayInMilliseconds)
+
+    return isVisible && <img {...props} />
+}
+
+export default LazyImage
